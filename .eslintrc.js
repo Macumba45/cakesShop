bff071844327bed15a37298b933a6d23fd16468a
Macumba45 <gonzalolovo@gmail.com>
2023-06-22 21:40:02 +0200

    docs: testing eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,35 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: ['standard-with-typescript', 'plugin:react/recommended'],
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: ['*.ts', '*.tsx'],
-            parserOptions: {
-                sourceType: 'script',
-            },
-        },
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: './tsconfig.json',
-    },
-    plugins: ['react'],
-    rules: {
-        'react/prop-types': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'no-unused-vars': 'warn',
-        semi: 'off',
-    },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'warn',
+    semi: 'off'
+  }
 }
